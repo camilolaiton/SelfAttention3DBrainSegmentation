3242d018aa7d31060b5cfa6d12f6ea16e5296494
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-07-13 10:07:46 -0500

    starting model scripts

---
+++ model/model.py
@@ -0,0 +1,22 @@
+"""
+    Made by:
+        - Camilo Laiton
+        Universidad Nacional de Colombia, Colombia
+        2021-1
+        GitHub: https://github.com/camilolaiton/
+
+        This file belongs to the private repository "master_thesis" where
+        I save all the files that are related to my thesis which is called
+        "Método para la segmentación de imágenes de resonancia magnética 
+        cerebrales usando una arquitectura de red neuronal basada en modelos
+        de atención".
+"""
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+# import tensorflow_addons as tfa
+
+def model():
+    pass

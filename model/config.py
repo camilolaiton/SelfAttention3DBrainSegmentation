985e7d11b8bda0935a102309309cbd471278bd4a
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-07-25 14:40:34 -0500

    creating lite and heavy architectures with residual connections

--- model/config.py
+++ model/config.py
@@ -1,8 +1,8 @@
 import ml_collections
 
-def get_initial_config():
+def get_config_1():
     """
-        Returns the transformer initial configuration
+        Returns the transformer configuration 1
     """
 
     config = ml_collections.ConfigDict()
@@ -16,6 +16,7 @@ def get_initial_config():
     config.image_width = 256
     config.image_channels = 1
     config.image_size = (config.image_height, config.image_width, config.image_channels)
+    config.residual_blocks = True
     
     config.transformer = ml_collections.ConfigDict()
     config.transformer.patch_size = 16
@@ -26,8 +27,45 @@ def get_initial_config():
         config.transformer.projection_dim, # 512
         # config.transformer.projection_dim, # 64
     ]
-    config.transformer.layers = 12
+    config.transformer.layers = 8
     config.transformer.num_heads = 12
+    config.transformer.dropout_rate = 0.1
+    config.transformer.normalization_rate = 1e-6
+
+    config.n_classes = 1
+    config.activation = 'softmax'
+
+    return config
+
+def get_config_2():
+    """
+        Returns the transformer configuration 2
+    """
+
+    config = ml_collections.ConfigDict()
+    config.dataset_path = 'dataset/'
+    config.learning_rate = 0.01
+    config.weight_decay = 1e-4
+    config.momentum = 0.9
+    config.batch_size = 256
+    config.num_epochs = 10
+    config.image_height = 256
+    config.image_width = 256
+    config.image_channels = 1
+    config.image_size = (config.image_height, config.image_width, config.image_channels)
+    config.residual_blocks = True
+
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.patch_size = 16
+    config.transformer.num_patches = (config.image_size[0] // config.transformer.patch_size)**2
+    config.transformer.projection_dim = 1024
+    config.transformer.units = [
+        config.transformer.projection_dim * 3, # (3) 3072 --  (4) 4096
+        config.transformer.projection_dim, # 512
+        # config.transformer.projection_dim, # 64
+    ]
+    config.transformer.layers = 8
+    config.transformer.num_heads = 8
     config.transformer.dropout_rate = 0.1
     config.transformer.normalization_rate = 1e-6
 
--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import Model, Input
 from tensorflow.keras.callbacks import ModelCheckpoint
 from blocks import *
-from config import get_initial_config
+from config import get_config_1, get_config_2
 import tensorflow_addons as tfa
 from metrics import dice_coefficient, mean_iou
 
@@ -15,7 +15,9 @@ def build_model(config):
         name='encoded_patches_0',    
     )(patches)
 
-    print(f"enconded patches size {encoded_patches.shape}")
+    # print(f"enconded patches size {encoded_patches.shape}")
+
+    transformer_blocks = []
     for idx in range(config.transformer.layers):
         # print(f"Starting {idx} iteration")
         encoded_patches = TransformerBlock(
@@ -26,6 +28,7 @@ def build_model(config):
             transformer_units=config.transformer.units, 
             name="transformer_block_"+str(idx)
         )(encoded_patches)
+        transformer_blocks.append(encoded_patches)
 
     x = None
     filters=16
@@ -37,6 +40,15 @@ def build_model(config):
                 normalization_rate=config.transformer.normalization_rate,
                 name=f'decoder_cup_{idx}'
             )(encoded_patches)
+
+            if (config.residual_blocks):
+                residual_block = DecoderBlockCup(
+                    target_shape=(config.transformer.projection_dim, config.image_height//16, config.image_width//16),
+                    filters=filters,
+                    normalization_rate=config.transformer.normalization_rate,
+                    name=f'decoder_cup_residual_{idx}'
+                )(transformer_blocks[int(len(transformer_blocks)//2) + 2])
+                x = layers.Add()([x, residual_block])
         else:
             filters *= 2
             x = DecoderUpsampleBlock(
@@ -44,14 +56,120 @@ def build_model(config):
                 name=f'decoder_upsample_{idx-1}'
             )(x)
 
+            if (config.residual_blocks):
+                if (idx == 2):
+                    residual_block = layers.Reshape(target_shape=(128, 32, 32))(transformer_blocks[0])
+                    residual_block = DecoderUpsampleBlock(
+                        filters=filters,
+                        name=f'decoder_upsample_residual_{idx}'
+                    )(residual_block)
+                    x = layers.Add()([x, residual_block])
+                
+                elif (idx == 1):
+                    residual_block = layers.Reshape(target_shape=(128, 32, 32))(transformer_blocks[int(len(transformer_blocks)//2)-1])
+                    x = layers.Add()([x, residual_block])
+
+
     x = DecoderSegmentationHead(
         name='decoder_segmentation_end_0'
     )(x)
 
+
+    # for idx in range(len(transformer_blocks)):
+    #     print(f"{transformer_blocks[idx].name}: {transformer_blocks[idx].shape}")
+
     return Model(inputs=inputs, outputs=x)
 
-def props(cls):   
-  return [i for i in cls.__dict__.keys() if i[:1] != '_']
+def build_model_2(config):
+    inputs = Input(shape=config.image_size)
+    patches = Patches(config.transformer.patch_size, name="patches_0")(inputs)
+    encoded_patches = PatchEncoder(
+        num_patches=config.transformer.num_patches, 
+        projection_dim=config.transformer.projection_dim,
+        name='encoded_patches_0',    
+    )(patches)
+
+    # print(f"enconded patches size {encoded_patches.shape}")
+
+    transformer_blocks = []
+    for idx in range(config.transformer.layers):
+        # print(f"Starting {idx} iteration")
+        encoded_patches = TransformerBlock(
+            num_heads=config.transformer.num_heads, 
+            projection_dim=config.transformer.projection_dim, 
+            dropout_rate=config.transformer.dropout_rate, 
+            normalization_rate=config.transformer.normalization_rate, 
+            transformer_units=config.transformer.units, 
+            name="transformer_block_"+str(idx)
+        )(encoded_patches)
+        transformer_blocks.append(encoded_patches)
+
+    x = None
+    filters=8
+    for idx in range(6):
+        if not idx:
+            x = DecoderBlockCup(
+                target_shape=(config.transformer.projection_dim, config.image_height//16, config.image_width//16),
+                filters=filters,
+                normalization_rate=config.transformer.normalization_rate,
+                pool_size=(2,2),
+                name=f'decoder_cup_{idx}'
+            )(encoded_patches)
+
+            if (config.residual_blocks):
+                residual_block = DecoderBlockCup(
+                    target_shape=(config.transformer.projection_dim, config.image_height//16, config.image_width//16),
+                    filters=filters,
+                    normalization_rate=config.transformer.normalization_rate,
+                    pool_size=(2, 2),
+                    name=f'decoder_cup_residual_{idx}'
+                )(transformer_blocks[int(len(transformer_blocks)//2)+2])
+                # print(residual_block.shape, " ", x.shape)
+                # residual_block = DecoderBlockCup(
+                #     target_shape=(config.transformer.projection_dim, config.image_height//16, config.image_width//16),
+                #     filters=filters,
+                #     normalization_rate=config.transformer.normalization_rate,
+                #     name=f'decoder_cup_residual_{idx}'
+                # )(x)
+
+                x = layers.Add()([x, residual_block])
+        else:
+            filters *= 2
+            x = DecoderUpsampleBlock(
+                filters=filters,
+                name=f'decoder_upsample_{idx-1}'
+            )(x)
+
+            if (config.residual_blocks):
+                if (idx == 2):
+                    residual_block = DecoderBlockCup(
+                        target_shape=(config.transformer.projection_dim, config.image_height//16, config.image_width//16),
+                        filters=filters/2,
+                        normalization_rate=config.transformer.normalization_rate,
+                        pool_size=(4, 1),
+                        name=f'decoder_cup_residual_{idx}'
+                    )(transformer_blocks[int(len(transformer_blocks)//2)-1])
+                    
+                    residual_block = DecoderUpsampleBlock(
+                        filters=filters,
+                        name=f'decoder_upsample_residual_{idx}'
+                    )(residual_block)
+                    # print(residual_block.shape, " ", x.shape)
+
+                    x = layers.Add()([x, residual_block])
+
+                elif (idx == 3):
+                    residual_block = layers.Reshape(target_shape=(64, 64, 64))(transformer_blocks[0])
+                    x = layers.Add()([x, residual_block])
+
+    x = DecoderSegmentationHead(
+        name='decoder_segmentation_end_0'
+    )(x)
+
+    for idx in range(len(transformer_blocks)):
+        print(f"{transformer_blocks[idx].name}: {transformer_blocks[idx].shape}")
+
+    return Model(inputs=inputs, outputs=x)
 
 def plot_model(model, input_shape=(None, 256, 256, 1)):
     all_layers = []
@@ -76,22 +194,40 @@ def plot_model(model, input_shape=(None, 256, 256, 1)):
     )
 
 if __name__ == "__main__":
-    config = get_initial_config()
-    model = build_model(config)
+    config_num = 2
+    architecture_image_name = f"model/model_architecture_skip_connection_{config_num}.png"
+    config = None
+    model = None
+
+    if (config_num == 1):
+        config = get_config_1()
+    else:
+        config = get_config_2()
+
+    print(f"[+] Building model {config_num} with config {config}")
+
+    if (config_num == 1):
+        model = build_model(config)
+    else:
+        model = build_model_2(config)
+    
     model.summary()
 
-    # tf.keras.utils.plot_model(
-    #     model,
-    #     to_file="model/model_architecture.png",
-    #     show_shapes=True,
-    #     show_dtype=True,
-    #     show_layer_names=True,
-    #     rankdir="TB",
-    #     expand_nested=False,
-    #     dpi=96,
-    # )
+    if not (config.residual_blocks):
+        architecture_image_name = architecture_image_name.replace("_skip_connection", "")
+    
+    tf.keras.utils.plot_model(
+        model,
+        to_file=architecture_image_name,
+        show_shapes=True,
+        show_dtype=True,
+        show_layer_names=True,
+        rankdir="TB",
+        expand_nested=False,
+        dpi=96,
+    )
 
-    plot_model(model)
+    # plot_model(model)
 
     optimizer = tf.optimizers.SGD(
         learning_rate=config.learning_rate, 

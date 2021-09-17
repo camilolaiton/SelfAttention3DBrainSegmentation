7a63b2ceecc8e1dd1f8b64e03acadfa489190fc0
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-09-17 11:03:38 -0500

    changes in config patchsize 32

--- model/config.py
+++ model/config.py
@@ -51,7 +51,7 @@ def get_config_32():
     config.learning_rate = 0.001
     config.weight_decay = 1e-4
     config.momentum = 0.9
-    config.batch_size = 2
+    config.batch_size = 1
     config.num_epochs = 20
     config.image_height = 256
     config.image_width = 256
@@ -66,12 +66,12 @@ def get_config_32():
     config.transformer.num_patches = (config.image_size[0] // config.transformer.patch_size)**3
     config.transformer.projection_dim = 128
     config.transformer.units = [
-        config.transformer.projection_dim * 3, # (3) 1536 --  (4) 2048
+        config.transformer.projection_dim * 2, # (3) 1536 --  (4) 2048
         config.transformer.projection_dim, # 512
         # config.transformer.projection_dim, # 64
     ]
-    config.transformer.layers = 5
-    config.transformer.num_heads = 4
+    config.transformer.layers = 4
+    config.transformer.num_heads = 3
     config.transformer.dropout_rate = 0.1
     config.transformer.normalization_rate = 1e-6
 

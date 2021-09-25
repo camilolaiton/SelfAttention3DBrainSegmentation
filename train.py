79b4f0a117d1fac262cab162f94ae20beeb86316
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-09-25 10:40:46 -0500

    testing only training

--- train.py
+++ train.py
@@ -355,7 +355,7 @@ def main():
     # )
 
     # reading for training
-    half = int(len(image_list_train)*0.7)
+    half = int(len(image_list_train)*0.5)
     train_imgs = utils.read_files_from_directory(image_list_train, half)
     train_msks = utils.read_files_from_directory(mask_list_train, half)
 

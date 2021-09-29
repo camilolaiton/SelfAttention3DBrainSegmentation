76f78d1bbfc8862b39fb9354b829bfc6bf2546c5
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-09-29 17:28:18 -0500

    creating new dataset with 64 patches inside a file

--- create_new_dataset.py
+++ create_new_dataset.py
@@ -12,10 +12,11 @@ def generating_images(patch_size, ori_path, dest_path):
         np_file_patches = np.expand_dims(np_file_patches, axis=4)
         name = filename.split('/')[-1] # .split('.')[0]
         name = name.split('\\')[-1].split('.')[0]
-
         print("Saving imgs patches for ", name)
-        for idx in range(np_file_patches.shape[0]):
-            np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
+        np.save(dest_path + f"/{name}-patchified.npy", np_file_patches)
+        
+        # for idx in range(np_file_patches.shape[0]):
+        #     np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
 
 def generating_msks(patch_size, ori_path, dest_path, num_classes):
     for filename in glob.glob(ori_path):
@@ -26,14 +27,16 @@ def generating_msks(patch_size, ori_path, dest_path, num_classes):
         np_file_patches = to_categorical(np_file_patches, num_classes=num_classes)
         name = filename.split('/')[-1] # .split('.')[0]
         name = name.split('\\')[-1].split('.')[0]
-
         print("Saving masks patches for ", name)
-        for idx in range(np_file_patches.shape[0]):
-            np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
+        np.save(dest_path + f"/{name}-patchified.npy", np_file_patches)
+        # for idx in range(np_file_patches.shape[0]):
+        #     np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
 
 def main():
     patch_size = 64
     num_classes = 4
+
+    dataset_name_folder = 'dataset_3D_2'
 
     train_dir_imgs = 'dataset_3D/train/images/*'
     train_dir_msks = 'dataset_3D/train/masks/*'
@@ -41,11 +44,11 @@ def main():
     test_dir_imgs = 'dataset_3D/test/images/*'
     test_dir_msks = 'dataset_3D/test/masks/*'
 
-    dest_train_dir_imgs = 'dataset_3D_2/train/images'
-    dest_train_dir_msks = 'dataset_3D_2/train/masks'
+    dest_train_dir_imgs = dataset_name_folder + '/train/images'
+    dest_train_dir_msks = dataset_name_folder + '/train/masks'
 
-    dest_test_dir_imgs = 'dataset_3D_2/test/images'
-    dest_test_dir_msks = 'dataset_3D_2/test/masks'
+    dest_test_dir_imgs = dataset_name_folder + '/test/images'
+    dest_test_dir_msks = dataset_name_folder + '/test/masks'
 
     for folder in [
         dest_train_dir_imgs, 
@@ -66,7 +69,6 @@ def main():
     generating_msks(patch_size, train_dir_msks, dest_train_dir_msks, num_classes)
     print("\n[+] Generating patched masks for test")
     generating_msks(patch_size, test_dir_msks, dest_test_dir_msks, num_classes)
-
 
 if __name__ == '__main__':
     main()

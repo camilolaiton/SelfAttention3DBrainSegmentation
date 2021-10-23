c2b9b9cd22128bc20703779dfc65706ccfbbecbd
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-10-22 21:55:33 -0500

    retrain

--- train.py
+++ train.py
@@ -324,7 +324,7 @@ def main():
             # Virtual devices must be set before GPUs have been initialized
             print(e)
 
-    retrain = False
+    retrain = True
     training_folder = 'trainings/version_22_0_2paths_dice_focal_up'
     model_path = f"{training_folder}/model_trained_architecture.hdf5"
     # model_path = f"{training_folder}/checkpoints_4/model_trained_09_0.68.hdf5"

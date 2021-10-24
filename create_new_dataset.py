29d9778f227b72c70558810c3ab6ad7e4f522190
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-10-24 17:19:59 -0500

    new dataset

--- create_new_dataset.py
+++ create_new_dataset.py
@@ -5,21 +5,31 @@ from patchify import patchify, unpatchify
 from tensorflow.keras.utils import to_categorical
 
 def generating_images(patch_size, ori_path, dest_path):
+    ij = 0
     for filename in glob.glob(ori_path):
+        if (ij == 1):
+            print("Exits with ", ij)
+            break
         np_file = np.load(filename)
+        print(np_file.shape)
         np_file_patches = patchify(np_file, (patch_size, patch_size, patch_size), step=patch_size)
         np_file_patches = np.reshape(np_file_patches, (-1, np_file_patches.shape[3], np_file_patches.shape[4], np_file_patches.shape[5]))
         np_file_patches = np.expand_dims(np_file_patches, axis=4)
+        print(np_file_patches.shape)
         name = filename.split('/')[-1] # .split('.')[0]
         name = name.split('\\')[-1].split('.')[0]
         print("Saving imgs patches for ", name)
         np.save(dest_path + f"/{name}-patchified.npy", np_file_patches)
-        
+        ij+=1
         # for idx in range(np_file_patches.shape[0]):
         #     np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
 
 def generating_msks(patch_size, ori_path, dest_path, num_classes):
+    i = 0
     for filename in glob.glob(ori_path):
+        if (i == 1):
+            print("Exits with ", i)
+            break
         np_file = np.argmax(np.load(filename), axis=3)
         np_file_patches = patchify(np_file, (patch_size, patch_size, patch_size), step=patch_size)
         np_file_patches = np.reshape(np_file_patches, (-1, np_file_patches.shape[3], np_file_patches.shape[4], np_file_patches.shape[5]))
@@ -29,6 +39,7 @@ def generating_msks(patch_size, ori_path, dest_path, num_classes):
         name = name.split('\\')[-1].split('.')[0]
         print("Saving masks patches for ", name)
         np.save(dest_path + f"/{name}-patchified.npy", np_file_patches)
+        i+=1
         # for idx in range(np_file_patches.shape[0]):
         #     np.save(dest_path + f"/{name}-patch-{idx}.npy", np_file_patches[idx, :, :, :, :])
 
@@ -58,15 +69,16 @@ def main():
     ]:
         utils.create_folder(folder)
 
-    # Generating images
+    # Generating images and masks for training
     print("\n[+] Generating patched images for training")
     generating_images(patch_size, train_dir_imgs, dest_train_dir_imgs)
-    print("\n[+] Generating patched images for test")
-    generating_images(patch_size, test_dir_imgs, dest_test_dir_imgs)
-
-    # Generating masks
     print("\n[+] Generating patched masks for training")
     generating_msks(patch_size, train_dir_msks, dest_train_dir_msks, num_classes)
+
+
+    # Generating images and masks for test
+    print("\n[+] Generating patched images for test")
+    generating_images(patch_size, test_dir_imgs, dest_test_dir_imgs)
     print("\n[+] Generating patched masks for test")
     generating_msks(patch_size, test_dir_msks, dest_test_dir_msks, num_classes)
 
--- main.py
+++ main.py
@@ -30,6 +30,9 @@ import nilearn
 import tensorflow as tf
 from model.config import *
 from volumentations import *
+from model.blocks import Keras3DAugmentation
+import nibabel as nib
+from patchify import patchify, unpatchify
 
 def structure_validation():
 
@@ -202,77 +205,110 @@ def main():
 
     # print("List: ", results_list)
 
+    # config = get_config_test()
+    # model = test_model_3(config)
+
+    # optimizer = tf.optimizers.SGD(
+    #     learning_rate=config.learning_rate, 
+    #     momentum=config.momentum,
+    #     name='optimizer_SGD_0'
+    # )
+
+    # model.compile(
+    #     optimizer=optimizer,
+    #     loss="categorical_crossentropy",#loss,#tversky_loss,
+    #     metrics=[
+    #         # 'accuracy',
+    #         sm.metrics.IOUScore(threshold=0.5),
+    #         sm.metrics.FScore(threshold=0.5),
+    #     ],
+    # )
+    # print(f"[+] Building model with config {config}")
+    # model.summary()
+    # tf.keras.utils.plot_model(
+    #     model,
+    #     to_file="test_model.png",
+    #     show_shapes=True,
+    #     show_dtype=True,
+    #     show_layer_names=True,
+    #     rankdir="TB",
+    #     expand_nested=False,
+    #     dpi=96,
+    # )
     # exit()
-    config = get_config_test()
-    model = test_model_3(config)
 
-    optimizer = tf.optimizers.SGD(
-        learning_rate=config.learning_rate, 
-        momentum=config.momentum,
-        name='optimizer_SGD_0'
-    )
+    # image_list_train = sorted(glob.glob(
+    #     config.dataset_path + 'train/images/*'))
+    # mask_list_train = sorted(glob.glob(
+    #     config.dataset_path + 'train/masks/*'))
+    # print(config.dataset_path, " ", len(image_list_train), " ", len(mask_list_train))
+    # image_list_test = sorted(glob.glob(
+    #     config.dataset_path + 'test/images/*'))
+    # mask_list_test = sorted(glob.glob(
+    #     config.dataset_path + 'test/masks/*'))
 
-    model.compile(
-        optimizer=optimizer,
-        loss="categorical_crossentropy",#loss,#tversky_loss,
-        metrics=[
-            # 'accuracy',
-            sm.metrics.IOUScore(threshold=0.5),
-            sm.metrics.FScore(threshold=0.5),
-        ],
-    )
-    print(f"[+] Building model with config {config}")
-    model.summary()
-    tf.keras.utils.plot_model(
-        model,
-        to_file="test_model.png",
-        show_shapes=True,
-        show_dtype=True,
-        show_layer_names=True,
-        rankdir="TB",
-        expand_nested=False,
-        dpi=96,
-    )
-    exit()
+    # # train_imgs = read_files_from_directory(image_list_train)
+    # # train_msks = read_files_from_directory(mask_list_train)
 
-    image_list_train = sorted(glob.glob(
-        config.dataset_path + 'train/images/*'))
-    mask_list_train = sorted(glob.glob(
-        config.dataset_path + 'train/masks/*'))
-    print(config.dataset_path, " ", len(image_list_train), " ", len(mask_list_train))
-    image_list_test = sorted(glob.glob(
-        config.dataset_path + 'test/images/*'))
-    mask_list_test = sorted(glob.glob(
-        config.dataset_path + 'test/masks/*'))
+    # # print(train_imgs.shape, "  ", train_msks.shape)
 
-    # train_imgs = read_files_from_directory(image_list_train)
-    # train_msks = read_files_from_directory(mask_list_train)
+    # train_dataset = tf.data.Dataset.from_tensor_slices(
+    #     # (train_imgs, train_msks)
+    #     (image_list_train, 
+    #     mask_list_train)
+    # )
 
-    # print(train_imgs.shape, "  ", train_msks.shape)
+    # dataset = {
+    #     "train" : train_dataset,
+    #     # "val" : val_dataset
+    # }
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(
-        # (train_imgs, train_msks)
-        (image_list_train, 
-        mask_list_train)
-    )
+    # AUTOTUNE = tf.data.experimental.AUTOTUNE
+    # BATCH_SIZE = 1
 
-    dataset = {
-        "train" : train_dataset,
-        # "val" : val_dataset
-    }
+    # options = tf.data.Options()
+    # options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
 
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    BATCH_SIZE = 8
+    # aug_layer = Keras3DAugmentation(
+    #     12, 
+    #     config.image_width, 
+    #     config.image_height, 
+    #     config.image_channels, 
+    #     name='data_aug'
+    # )
 
-    dataset['train'] = dataset['train'].map(load_files)
-    if (config.unbatch):
-        dataset['train'] = dataset['train'].unbatch()
-    # dataset['train'] = dataset['train'].map(augmentor, num_parallel_calls=AUTOTUNE)
-    dataset['train'] = dataset['train'].repeat()
-    dataset['train'] = dataset['train'].batch(BATCH_SIZE)
-    dataset['train'] = dataset['train'].prefetch(AUTOTUNE)
-    see = next(iter(dataset['train']))
-    print("SEE: ", see, " ", len(see))
+    # dataset['train'] = dataset['train'].map(load_files)
+    # if (config.unbatch):
+    #     dataset['train'] = dataset['train'].unbatch()
+    # # dataset['train'] = dataset['train'].map(augmentor, num_parallel_calls=AUTOTUNE)
+    # dataset['train'] = dataset['train'].repeat()
+    # dataset['train'] = dataset['train'].batch(BATCH_SIZE)
+    # dataset['train'] = dataset['train'].prefetch(AUTOTUNE)
+    # dataset['train'] = dataset['train'].with_options(options)
+    # dataset['train'] = dataset['train'].map(lambda x, y: (aug_layer(x),y), num_parallel_calls=AUTOTUNE)
+    # x, y = next(iter(dataset['train']))
+    
+    # print(x.shape, " ", y.shape, " ", np.unique(np.argmax(y, axis=4)))
+
+    # # plt.figure(figsize=(30, 20))
+    
+    # # for j in range(config.image_height):
+    # # plt.subplot(8, 8, j + 1)
+    # watch = x[0, 32, :, :, 0]
+    # orig = np.load('dataset_3D/train/images/HLN-12-1.npy')
+    # print(watch.shape)
+    # plt.imshow(watch, cmap='gray')
+    # plt.axis("off")
+    # plt.title('test')
+    # plt.show()
+
+    # print(orig[:128,128,:128].shape)
+    # plt.imshow(orig[:128,128,:128], cmap='gray')
+    # plt.axis("off")
+    # plt.title('test')
+    # plt.show()
+
+    # exit()
     # dataset['val'] = dataset['val'].from_generator(val_datagen)
     # dataset['val'] = dataset['val'].repeat()
     # dataset['val'] = dataset['val'].batch(BATCH_SIZE)
@@ -281,12 +317,7 @@ def main():
     # print(dataset['train'])
     # print(dataset['val'])
     # test_tf_function()
-    STRUCTURES = utils.read_test_to_list('data/common_anatomical_structures.txt')
-    mri_paths = utils.read_test_to_list('data/common_mri_images.txt')
-    
-    for idx in range(len(mri_paths)):
-        tmp = mri_paths[idx].split('-')
-        mri_paths[idx] = f"data/{'-'.join(tmp[:-1])}/{mri_paths[idx]}"
+
 
     # data_img, data = utils.readMRI(mri_paths[0] + '/001.mgz', config_orig)
     # data_msk_img, data_msk = utils.readMRI(mri_paths[0] + '/aparcNMMjt+aseg.mgz', config_msk)
@@ -304,37 +335,104 @@ def main():
     #     print(np.unique(data))
     #     # print(data_img.shape, "  ", np.unique(data))
 
-    # utils.create_folder('dataset_3D/images')
-    # utils.create_folder('dataset_3D/masks')
-
     # # start_time = time.time()
     # # MMRR-21-3
+    """
+    STRUCTURES = utils.read_test_to_list('data/common_anatomical_structures.txt')
+    mri_paths = utils.read_test_to_list('data/common_mri_images.txt')
+    
+    for idx in range(len(mri_paths)):
+        tmp = mri_paths[idx].split('-')
+        mri_paths[idx] = f"data/{'-'.join(tmp[:-1])}/{mri_paths[idx]}"
 
-    # mri_paths = mri_paths[mri_paths.index('data/HLN-12/HLN-12-6'):mri_paths.index('data/HLN-12/HLN-12-6')+1]
+    print(mri_paths[0], len(mri_paths))
+
+    for folder in ['train', 'test']:
+        utils.create_folder(f"dataset_3D/{folder}/images")
+        utils.create_folder(f"dataset_3D/{folder}/masks")
+
+    mri_paths = mri_paths[:2] + [mri_paths[mri_paths.index('data/HLN-12/HLN-12-1')]]
     # print(mri_paths)
-    # for mri_path in mri_paths:
+    for mri_path in mri_paths:
 
-    #     name = mri_path.split('/')[-1]
+        name = mri_path.split('/')[-1]
         
-    #     data_img, data = utils.readMRI(mri_path + '/001.mgz', config_orig)
-    #     data_msk_img, data_msk = utils.readMRI(mri_path + '/aparcNMMjt+aseg.mgz', config_msk)
-    #     if data.shape != data_msk.shape:
-    #         print("Fixing shapes in: ", name)
-    #         data_img = nilearn.image.resample_to_img(data_img, data_msk_img)
-    #         data = data_img.get_fdata()
-    #         data = scaler.fit_transform(data.reshape(-1, data.shape[-1])).reshape(data.shape)
+        data_img, data = utils.readMRI(mri_path + '/001.mgz', config_orig)
+        data_msk_img, data_msk = utils.readMRI(mri_path + '/aparcNMMjt+aseg.mgz', config_msk)
+        print("after read: ", nib.aff2axcodes(data_img.affine))
+        if data.shape != data_msk.shape:
+            print("Fixing shapes in: ", name)
+            data_img = nilearn.image.resample_to_img(data_img, data_msk_img)
+            data = data_img.get_fdata()
+            data = scaler.fit_transform(data.reshape(-1, data.shape[-1])).reshape(data.shape)
 
-    #     msk = np.zeros((256, 256, 256), dtype=np.uint8)
-    #     for structure in STRUCTURES:
-    #         msk = helper_anat_structure(msk, data_msk, lut_file[structure], class_info[structure]['new_id'])
+        msk = np.zeros((256, 256, 256), dtype=np.uint8)
+        for structure in STRUCTURES:
+            msk = helper_anat_structure(msk, data_msk, lut_file[structure], class_info[structure]['new_id'])
 
-    #     msk = to_categorical(msk, num_classes=4)
-    #     # Saving normalized mri
-    #     np.save(f'dataset_3D/test/images/{name}.npy', data)
+        msk = msk[45:215, 40:210, 30:200]
+        msk = to_categorical(msk, num_classes=4)
+        # Saving normalized mri
+        np.save(f'dataset_3D/test/images/{name}.npy', data[45:215, 40:210, 30:200])
 
-    #     # Saving msk
-    #     np.save(f'dataset_3D/test/masks/{name}.npy', msk)
+        # Saving msk
+        np.save(f'dataset_3D/test/masks/{name}.npy', msk)
+    # exit()
+    """
+    def plot_np_file(np_file, np_msk, view, name):
+        shapes = np_file.shape        
+        
+        for xj in range(0, 150, 15):
+            plt.figure(figsize=(30, 20))
+            plt.suptitle(f"{name} - {view}")
+            for x in range(0, 16):
+                # print(start, " ", x, " ", xj-start)
+                plt.subplot(4, 4, x + 1)
+                if (view == 'saggital' ):
+                    plt.imshow(np_file[x+xj, :, :], cmap='gray')
+                    plt.imshow(np_msk[x+xj, :, :], alpha=0.5)
+                elif (view == 'coronal' ):
+                    plt.imshow(np_file[:, x+xj, :], cmap='gray')
+                    plt.imshow(np_msk[:, x+xj, :], alpha=0.5)
+                else:
+                    plt.imshow(np_file[:, :, x+xj], cmap='gray')
+                    plt.imshow(np_msk[:, :, x+xj], alpha=0.5)
 
+                plt.axis("off")
+                plt.title(x+xj)
+            plt.show()
+
+    palette = np.array([[  0,   0,   0],   # black
+                [255,   0,   0],   # red
+                [  0, 255,   0],   # green
+                [  0,   0, 255]])   # blue
+                # [255, 255, 255]])  # white    
+    
+    patch_size = 85
+    for path in glob.glob('dataset_3D/test/images/*'):
+        file = np.load(path)
+        np_file_patches = patchify(file, (patch_size, patch_size, patch_size), step=patch_size)
+        np_file_patches = np.reshape(np_file_patches, (-1, np_file_patches.shape[3], np_file_patches.shape[4], np_file_patches.shape[5]))
+        print(np_file_patches.shape)
+
+        file_msk = np.argmax(np.load(path.replace('images', 'masks')), axis=3)
+        print("Before msk: ", np.unique(file_msk))
+        np_file_msk_patches = patchify(file_msk, (patch_size, patch_size, patch_size), step=patch_size)
+        np_file_msk_patches = np.reshape(np_file_msk_patches, (-1, np_file_msk_patches.shape[3], np_file_msk_patches.shape[4], np_file_msk_patches.shape[5]))
+        print("After msk: ", np.unique(np_file_msk_patches))
+        np_file_msk_patches = palette[np_file_msk_patches]
+        plt.imshow(np_file_patches[2, :, :, 68], cmap='gray')
+        plt.imshow(np_file_msk_patches[2, :, :, 68], alpha=0.5)
+        plt.axis("off")
+        plt.show()
+    #     name = path.split('/')[-1]
+        
+    #     # Adding colors
+    #     file_msk = palette[file_msk]
+
+    #     for view in ['saggital', 'coronal', 'axial']:
+    #         plot_np_file(file, file_msk, view, name)
+    exit()
     # # seconds = (time.time() - start_time)
     # # print("Processing time: ", seconds)
 

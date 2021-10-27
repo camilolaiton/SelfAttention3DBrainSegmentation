c21e713ddb80fde538ee1f707c29eb9a238011c4
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-10-26 22:48:34 -0500

    new testing with data aug

--- train.py
+++ train.py
@@ -329,7 +329,7 @@ def main():
             print(e)
 
     retrain = False
-    training_folder = 'trainings/version_31'
+    training_folder = 'trainings/version_32'
     model_path = f"{training_folder}/model_trained_architecture.hdf5"
     # model_path = f"{training_folder}/checkpoints_4/model_trained_09_0.68.hdf5"
 

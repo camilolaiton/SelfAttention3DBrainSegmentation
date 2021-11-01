bb519b2ab07688cb13c494dd61b4dfa43bfa62a2
Camilo Laiton <kmilo9713.cl@gmail.com>
2021-10-31 23:34:46 -0500

    new loss fnc

--- train.py
+++ train.py
@@ -355,7 +355,7 @@ def main():
             print(e)
 
     retrain = False
-    training_folder = 'trainings/version_34'
+    training_folder = 'trainings/version_35'
     model_path = f"{training_folder}/model_trained_architecture.hdf5"
     # model_path = f"{training_folder}/checkpoints_4/model_trained_09_0.68.hdf5"
 

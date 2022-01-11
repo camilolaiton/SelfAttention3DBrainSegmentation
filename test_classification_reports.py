d6790b389b080f68d832e8f6c5ae457a9ff1d737
Camilo Laiton <kmilo9713.cl@gmail.com>
2022-01-10 21:35:28 -0500

    improving test classification report

--- test_classification_reports.py
+++ test_classification_reports.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import argparse
 from sklearn.metrics import classification_report
+import time
 
 def read_patches_filename(filename, path):
     patches = []
@@ -49,17 +50,29 @@ def main():
     model = build_model(config)
     model_path = f"{training_folder}/model_trained_architecture.hdf5"
     model.load_weights(model_path)
+    times = {}
 
     for idx in range(len(image_list_test)):
-        print(f"Image path: {image_list_test[idx]} test path: {mask_list_test[idx]}")
+        print(f"[+] Image path: {image_list_test[idx]} test path: {mask_list_test[idx]}")
         filename = image_list_test[idx].split('/')[-1].split('.')[0]
         img_patches = np.load(image_list_test[idx])
         msk_patches = np.load(mask_list_test[idx])
+
+        start_time = time.time()
+        
+        print(f"[+] Starting prediction for {filename}")
         prediction = model.predict(img_patches)
+        end_time = time.time()
+        final_time = (end_time-start_time)/60
+        times[filename] = final_time
+        print(f"[+] Finished prediction for {filename} in {final_time} minutes")
+
         prediction = np.argmax(prediction, axis=4)
 
         report = classification_report(msk_patches.flatten(), prediction.flatten(), target_names=STRUCTURES)
         utils.classification_report_csv(report, training_folder + deep_folder, f"/{filename}_{len(STRUCTURES)}_structures")
+
+    utils.write_dict_to_txt(times, training_folder + deep_folder + '/times.txt')
 
 if __name__ == "__main__":
     main()

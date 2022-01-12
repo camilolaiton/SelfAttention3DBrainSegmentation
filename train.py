c7be54e6e4fb614775a629feef554c16405de1af
Camilo Laiton <kmilo9713.cl@gmail.com>
2022-01-12 00:18:04 -0500

    modifiting train code

--- train.py
+++ train.py
@@ -617,7 +617,7 @@ def main():
 
     print("[+] Num parallel calls: ", AUTOTUNE)
     
-    dataset['train'] = dataset['train'].shuffle(buffer_size=config.batch_size, seed=SEED)
+    # dataset['train'] = dataset['train'].shuffle(buffer_size=config.batch_size, seed=SEED)
     dataset['train'] = dataset['train'].map(load_files).map(augmentor, num_parallel_calls=AUTOTUNE)
     if (config.unbatch):
         dataset['train'] = dataset['train'].unbatch()
